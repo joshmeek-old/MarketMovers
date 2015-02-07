016ecb3c544731823a320ed98ad3890f87a775ab
Josh Meek <joshmeek18@gmail.com>
2015-02-07 01:17:44 -0500

    Finished basic file

--- script.py
+++ script.py
@@ -1,7 +1,17 @@
 import ystockquote
 import csv
 
-with open('symbols.csv', 'rb') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print row
+smallestValue = 0;
+
+def smallest( symbol, value ):
+    global smallestValue
+    if value < smallestValue:
+        smallestValue = value
+        print symbol + " " + str(value)
+
+while True:
+    with open('symbols.csv', 'rU') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if ystockquote.get_change(row[0]) != 'N/A':
+                smallest(row[0], float(ystockquote.get_change(row[0]).strip('+')))
